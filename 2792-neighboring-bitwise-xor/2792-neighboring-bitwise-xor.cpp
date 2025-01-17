870cccf2043870683be8e8de8abeeb67afb7d622
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2025-01-17 13:45:22 +0530

    Time: 0 ms (100%), Space: 263.9 MB (48.13%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     bool doesValidArrayExist(vector<int>& derived) {
-        int ans=0;
-        for (int i:derived){
-            ans^=i;
-        }   
-        return ans==0;
+        int ans = 0;
+        int n = derived.size();
+        for (int i = 0; i < n; ++i) {
+            ans = ans ^ derived[i];
+        }
+        return ans == 0;
     }
 };
