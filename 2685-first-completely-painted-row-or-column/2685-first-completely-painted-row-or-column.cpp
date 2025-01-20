de46b9c7f9cc3f8203fddeb07a49870f24dd506c
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2025-01-20 11:56:57 +0530

    Time: 118 ms (29.88%), Space: 171 MB (28.35%) - LeetHub

---
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
+
+        unordered_map<int, pair<int, int>> mp;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                mp[mat[i][j]] = {i, j};
+            }
+        }
+
+        vector<int> rows(m, 0);
+        vector<int> cols(n, 0);
+
+        for (int i = 0; i < n * m; ++i) {
+            int r = mp[arr[i]].first;
+            int c = mp[arr[i]].second;
+
+            rows[r]++;
+            cols[c]++;
+
+            if (rows[r] == n || cols[c] == m) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
