6137feb702e5e5d8ba3b685199740b013aa8b6c9
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2025-01-22 21:56:18 +0530

    Time: 3 ms (75.09%), Space: 15.8 MB (9.8%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
 
-        unordered_map<int, int> mp;
+        map<int, int> mp;
 
         for (int i = 0; i < n; ++i) {
             if (mp[target - nums[i]] > 0)
