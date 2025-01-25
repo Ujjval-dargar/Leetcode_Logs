ad792c232684a5cdc85651fd34c0d26b89ed4d35
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2025-01-25 15:27:45 +0530

    Time: 687 ms (8.11%), Space: 376.1 MB (5.4%) - LeetHub

---
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/3219-make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
+        vector<int> ans;
+
+        int n = nums.size();
+        vector<int> sorted = nums;
+        sort(sorted.begin(), sorted.end());
+
+        map<int, queue<int>> group;
+        map<int, int> mp;
+
+        group[0].push(sorted[0]);
+        mp[sorted[0]] = 0;
+
+        int grp = 0;
+        for (int i = 1; i < n; ++i) {
+            if (sorted[i] - sorted[i - 1] > limit) {
+                grp++;
+            }
+            group[grp].push(sorted[i]);
+            mp[sorted[i]] = grp;
+        }
+
+        for (int i = 0; i < n; ++i) {
+
+            int t = group[mp[nums[i]]].front();
+            group[mp[nums[i]]].pop();
+            ans.push_back(t);
+        }
+
+        return ans;
+    }
+};
