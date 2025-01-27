44809107cbff41e9c0c25577934721ba872323eb
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2025-01-27 21:27:55 +0530

    Time: 144 ms (21.76%), Space: 69.9 MB (59.44%) - LeetHub

--- 1558-course-schedule-iv/1558-course-schedule-iv.cpp
+++ 1558-course-schedule-iv/1558-course-schedule-iv.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
+    bool dfs(unordered_map<int, vector<int>> &mp, int src, int end,
+             vector<bool>& visited) {
+        visited[src] = true;
+        if (src == end)
+            return true;
 
-    bool dfs(unordered_map<int,vector<int>> &m, int node, vector<int> &vis, int target){
-        if(node==target) return true;
-        vis[node] = 1;
-        for(auto i:m[node]){
-            if(vis[i]==-1){
-                if(dfs(m,i,vis,target)) return true;
-            }
+        for (int i : mp[src]) {
+            if (!visited[i] && dfs(mp, i, end, visited))
+                return true;
         }
         return false;
     }
 
-    vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& arr, vector<vector<int>>& queries) {
+    vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& arr,
+                                     vector<vector<int>>& q) {
+
+        unordered_map<int, vector<int>> mp;
+        for (int i = 0; i < arr.size(); ++i) {
+            mp[arr[i][0]].push_back(arr[i][1]);
+        }
+
         vector<bool> ans;
-        unordered_map<int,vector<int>> m;
-        for(auto i:arr){
-            m[i[0]].push_back(i[1]);
+        int m = q.size();
+        for (int i = 0; i < m; ++i) {
+            vector<bool> visited(n, false);
+            ans.push_back(dfs(mp, q[i][0], q[i][1], visited));
         }
-        for(auto i:queries){
-            vector<int> vis(n,-1);
-            ans.push_back(dfs(m,i[0],vis,i[1]));
-        }
+
         return ans;
     }
 };
