2b409c4ef9308672ef9f5505f04fef31b9171a45
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2025-02-10 13:56:29 +0530

    Time: 3 ms (15.28%), Space: 8.4 MB (87.14%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.cpp
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -3,11 +3,11 @@ public:
     string clearDigits(string s) {
         string ans = "";
         int n = s.size();
-        for (int i=0;i<n;++i){
+        for (int i = 0; i < n; ++i) {
             char c = s[i];
-            if (c>='0' && c<='9'){
+            if (c >= '0' && c <= '9') {
                 ans.pop_back();
-            }else {
+            } else {
                 ans.push_back(c);
             }
         }
