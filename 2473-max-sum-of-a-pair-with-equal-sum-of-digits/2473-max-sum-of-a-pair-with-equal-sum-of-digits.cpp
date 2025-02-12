ea4f6d8cc71dd4de54e5c74e362b1848a9690fc3
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2025-02-12 16:55:56 +0530

    Time: 42 ms (33.88%), Space: 64.1 MB (52.02%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -2,26 +2,51 @@ class Solution {
 public:
     int digitSum(int num) {
         int s = 0;
-        while (num > 0) {
+        while (num > 0) 
+        {
             s += num % 10;
             num = num / 10;
         }
         return s;
     }
 
-    int maximumSum(vector<int>& nums) {
+    int maximumSum(vector<int>& nums) 
+    {
         int n = nums.size();
-        unordered_map<int, vector<int>> mp;
-        sort(nums.begin(), nums.end());
-        for (int i = 0; i < n; ++i) {
-            mp[digitSum(nums[i])].push_back(nums[i]);
-        }
 
         int ans = -1;
-        for (auto i : mp) {
-            int m = i.second.size();
-            if (m >= 2) {
-                ans = max(i.second[m - 1] + i.second[m - 2], ans);
+
+        unordered_map<int, vector<int>> mp;
+
+        for (int i = 0; i < n; ++i) {
+
+            int d = digitSum(nums[i]);
+
+            if (mp[d].size() < 2) 
+            {
+                mp[d].push_back(nums[i]);
+            } 
+            else if (mp[d].size() == 2) 
+            {
+                int a = mp[d][0];
+                int b = mp[d][1];
+
+                if (nums[i] > min(a, b)) 
+                {
+                    if (a < b) 
+                    {
+                        mp[d][0] = nums[i];
+                    } 
+                    else 
+                    {
+                        mp[d][1] = nums[i];
+                    }
+                }
+            }
+
+            if (mp[d].size() == 2)
+            {
+                ans = max(ans, mp[d][0] + mp[d][1]);
             }
         }
 
