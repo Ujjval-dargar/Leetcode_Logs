234a29782ba91ce8ca5d5551c1b309f8cad037d2
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2025-02-13 19:32:38 +0530

    Time: 99 ms (47.48%), Space: 91.3 MB (90.45%) - LeetHub

--- 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.cpp
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -4,17 +4,19 @@ public:
         int n = nums.size();
         int ans = 0;
 
-        priority_queue<long long, vector<long long>, greater<long long>> pq(nums.begin(),
-                                                          nums.end());
+        priority_queue<long long, vector<long long>, greater<long long>> pq(
+            nums.begin(), nums.end());
 
-        while (pq.top() < k) {
-            long long a = pq.top();
+        long long a = pq.top();
+        while (a < k) {
             pq.pop();
             long long b = pq.top();
             pq.pop();
 
             ans++;
             pq.push(min(a, b) * 2 + max(a, b));
+
+            a = pq.top();
         }
 
         return ans;
