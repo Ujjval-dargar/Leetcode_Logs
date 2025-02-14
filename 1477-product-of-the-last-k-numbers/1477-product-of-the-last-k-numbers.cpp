9914e284d4042a43b98ff03e078d8c061c274c6e
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2025-02-14 13:46:28 +0530

    Time: 16 ms (82.06%), Space: 78 MB (77.84%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
@@ -4,15 +4,28 @@ public:
 
     ProductOfNumbers() {}
 
-    void add(int num) { nums.push_back(num); }
+    void add(int num) {
+        int n = nums.size();
+
+        if (n == 0) {
+            nums.push_back(num);
+        } else {
+            nums.push_back(num * nums[n - 1]);
+        }
+
+        if (num == 0) {
+            nums.clear();
+        }
+    }
 
     int getProduct(int k) {
         int n = nums.size();
-        int ans = 1;
-        for (int i = n - k; i < n; ++i) {
-            ans = ans * nums[i];
-        }
-        return ans;
+
+        if (n-1-k>=0)
+            return nums[n - 1] / nums[n - 1 - k];
+        else if (n-1-k==-1)
+            return nums[n-1];
+        return 0;
     }
 };
 
