b235cdb2e6a8244b579af6ac262311dbe17413e0
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2025-02-15 23:07:49 +0530

    Time: 0 ms (100%), Space: 8 MB (91.69%) - LeetHub

--- 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.cpp
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
-    int punishmentNumber(int n) {
-        vector<int> nums = {1,   9,   10,  36,  45,  55,  82,  91,  99,  100,
-                            235, 297, 369, 370, 379, 414, 657, 675, 703, 756,
-                            792, 909, 918, 945, 964, 990, 991, 999, 1000};
 
-        int ans = 0;
-        int s = nums.size();
-        for (int i = 0; i < s; ++i) {
-            if (nums[i] <= n)
-                ans += nums[i] * nums[i];
-            else
-                break;
+    bool check(int n, int k) {
+        if (n == k) {
+            return true;
+        }
+        if (k <= 0 || n < k) {
+            return false;
         }
 
+        return check(n / 10, k - n % 10) || 
+               check(n / 100, k - n % 100) ||
+               check(n / 1000, k - n % 1000);
+    }
+
+    int punishmentNumber(int n) {
+        int ans = 0;
+
+        for (int i = 1; i <= n; ++i) {
+            if (check(i * i, i)) {
+                ans += i * i;
+            }
+        }
         return ans;
     }
 };
