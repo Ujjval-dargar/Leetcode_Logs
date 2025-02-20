c732c318edf1387d0e2a0a74165e946308b4e0cb
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2025-02-20 15:34:43 +0530

    Time: 0 ms (100%), Space: 12.6 MB (84%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -1,52 +1,21 @@
 class Solution {
 public:
-    int bintoNum(string s) {
-        int ans = 0;
-        int n = s.size();
-        for (int i = 0; i < n; ++i) {
-            if (s[i] == '1') {
-                ans += pow(2, n - 1 - i);
-            }
-        }
-
-        return ans;
-    }
-
-    string numtoBin(int num) {
-        string ans = "";
-        while (num > 0) {
-            ans.push_back('0' + num % 2);
-            num = num / 2;
-        }
-
-        reverse(ans.begin(), ans.end());
-        return ans;
-    }
-
     string findDifferentBinaryString(vector<string>& nums) {
-        unordered_map<int, bool> mp;
-        int n = nums.size();
-        for (int i = 0; i < n; ++i) {
-            mp[bintoNum(nums[i])] = true;
-        }
-
         string ans;
+        int n=nums.size();
+        for (int i=0;i<n;++i){
+            ans.push_back('0');
+        }
 
-        for (int i = 0; i <= 16; ++i) {
-            if (!mp[i]) {
-                ans = numtoBin(i);
-                break;
+        for (int i=0;i<n;++i){
+            if (nums[i][i]=='0'){
+                ans[i]='1';
+            }
+            else {
+                ans[i]='0';
             }
         }
 
-        int k = ans.size();
-        string temp = "";
-        if (k < n) {
-            for (int i = k + 1; i <= n; ++i) {
-                temp.push_back('0');
-            }
-        }
-
-        return temp + ans;
+        return ans;
     }
 };
