99318e191ab4efff6ba62848097b42bd77fe7d30
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2025-02-21 11:00:51 +0530

    Time: 3 ms (94.21%), Space: 27.3 MB (80.56%) - LeetHub

--- 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.cpp
+++ 1387-find-elements-in-a-contaminated-binary-tree/1387-find-elements-in-a-contaminated-binary-tree.cpp
@@ -1,38 +1,30 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
 class FindElements {
+    unordered_set<int> seen;
+
 public:
-    unordered_map<int, bool> mp;
+    FindElements(TreeNode* root) { bfs(root); }
 
-    FindElements(TreeNode* root) { helper(root,0); }
+    bool find(int target) { return seen.find(target) != seen.end(); }
 
-    void helper(TreeNode* root, int val) {
-        if (root == nullptr) {
-            return;
+private:
+    void bfs(TreeNode* root) {
+        queue<TreeNode*> queue;
+        root->val = 0;
+        queue.push(root);
+
+        while (!queue.empty()) {
+            TreeNode* currentNode = queue.front();
+            queue.pop();
+            // visit currentNode by adding its recovered value to the set
+            seen.insert(currentNode->val);
+            if (currentNode->left) {
+                currentNode->left->val = currentNode->val * 2 + 1;
+                queue.push(currentNode->left);
+            }
+            if (currentNode->right) {
+                currentNode->right->val = currentNode->val * 2 + 2;
+                queue.push(currentNode->right);
+            }
         }
-
-        mp[val] = true;
-
-        root->val = val;
-        helper(root->left, 2 * val + 1);
-        helper(root->right, 2 * val + 2);
     }
-
-    bool find(int target) { return mp[target]; }
 };
-
-/**
- * Your FindElements object will be instantiated and called as such:
- * FindElements* obj = new FindElements(root);
- * bool param_1 = obj->find(target);
- */
