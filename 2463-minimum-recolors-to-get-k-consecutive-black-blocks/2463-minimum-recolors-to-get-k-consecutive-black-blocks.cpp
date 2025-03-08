e34cf95f2e5a3ca33cd8d337f4564531b78263ed
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2025-03-08 13:10:05 +0530

    Time: 0 ms (100%), Space: 8.3 MB (62.68%) - LeetHub

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -4,13 +4,6 @@ public:
         int n = blocks.size();
         int ans = 0;
 
-        // if (k==1){
-        //     for (int i=0;i<n;++i){
-        //         if (blocks[i]=='B')return 0;
-        //     }
-        //     return 1;
-        // }
-
         for (int i = 0; i < k; ++i) {
             ans += blocks[i] == 'W';
         }
@@ -20,7 +13,6 @@ public:
         int t = ans;
         while (j < n - 1) {
 
-            cout << i << " " << j << " " << ans << endl;
             if (blocks[i] == 'W') t--;
             if (blocks[j + 1] == 'W') t++;
             ans = min(ans, t);
