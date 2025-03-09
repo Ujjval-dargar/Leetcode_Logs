5f7b5531132e34b5c000b57751b962961cdb8551
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2025-03-09 16:09:37 +0530

    Time: 541 ms (5.02%), Space: 108.1 MB (38.7%) - LeetHub

---
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int numberOfAlternatingGroups(vector<int>& colors, int k) {
+        for (int i = 0; i < k-1; ++i) {
+            colors.push_back(colors[i]);
+        }
+
+        int n = colors.size();
+        int l = 0;
+        int r = 1;
+        int ans = 0;
+
+        while (r < n) {
+            if (colors[r - 1] == colors[r]) {
+                l = r;
+            }
+
+            if (r - l + 1 == k) {
+                cout<<l<<" "<<r<<endl;
+                ans++;
+                l++;
+            }
+
+            r++;
+        }
+
+        return ans;
+    }
+};
