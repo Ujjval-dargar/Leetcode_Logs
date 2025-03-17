dfa03f2f50ccbfbc4ed35f5f6ff6c84b7ee2d1a5
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2025-03-17 14:29:37 +0530

    Time: 0 ms (100%), Space: 13.3 MB (9.89%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int m = triangle.size();
+
+        vector<vector<int>> dp = triangle;
+
+        for (int i = 1; i < m; ++i) {
+            int n = triangle[i].size();
+            dp[i][0] = dp[i][0] + dp[i - 1][0];
+            dp[i][n - 1] = dp[i][n - 1] + dp[i - 1][n - 2];
+        }
+
+        for (int i = 2; i < m; ++i) {
+            int n = triangle[i].size();
+            for (int j = 1; j < n - 1; ++j) {
+                dp[i][j] = dp[i][j] + min(dp[i - 1][j - 1], dp[i - 1][j]);
+            }
+        }
+
+        return *min_element(dp[m - 1].begin(), dp[m - 1].end());
+    }
+};
