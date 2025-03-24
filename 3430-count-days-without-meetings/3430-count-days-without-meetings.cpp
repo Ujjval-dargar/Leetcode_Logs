c4fdb9fcce852bd5dac6337a5571506baf7d6af6
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2025-03-24 19:10:17 +0530

    Time: 69 ms (59.69%), Space: 126.7 MB (35.2%) - LeetHub

---
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<vector<int>> mergeInterval(vector<vector<int>>& arr) {
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+
+        int l = arr[0][0];
+        int r = arr[0][1];
+
+        vector<vector<int>> ans;
+
+        for (int i = 1; i < n; ++i) {
+            int tl = arr[i][0];
+            int tr = arr[i][1];
+
+            if (tl <= r && tr > r) {
+                r = tr;
+
+            } else if (tl > r) {
+                ans.push_back({l, r});
+                l = tl;
+                r = tr;
+            }
+        }
+        ans.push_back({l, r});
+
+        return ans;
+    }
+
+    int countDays(int days, vector<vector<int>>& meetings) {
+        vector<vector<int>> merged = mergeInterval(meetings);
+        int n = merged.size();
+
+        int d = 0;
+
+        for (int i = 0; i < n; ++i) {
+            // cout << merged[i][0] << " " << merged[i][1] << endl;
+            d += merged[i][1] - merged[i][0] + 1;
+        }
+
+        return days - d;
+    }
+};
