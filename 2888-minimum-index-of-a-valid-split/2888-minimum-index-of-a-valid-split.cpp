cc9261f67a1f6aea2dc0af7945c8eba897a7f2ae
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2025-03-28 02:03:55 +0530

    Time: 2406 ms (5.39%), Space: 110.9 MB (5.39%) - LeetHub

---
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int minimumIndex(vector<int>& arr) {
+
+        int n = arr.size();
+        vector<int> p1(n, -1);
+        vector<int> p2(n, -1);
+
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < n; ++i) {
+            mp[arr[i]]++;
+            if (mp[arr[i]] > (i + 1) / 2) {
+                p1[i] = arr[i];
+            } else {
+                int t = p1[i - 1];
+                if (mp[t] > (i + 1) / 2) {
+                    p1[i] = t;
+                }
+            }
+        }
+
+        mp.clear();
+        reverse(arr.begin(),arr.end());
+        for (int i = 0; i < n; ++i) {
+            mp[arr[i]]++;
+            if (mp[arr[i]] > (i + 1) / 2) {
+                p2[i] = arr[i];
+            } else {
+                int t = p2[i - 1];
+                if (mp[t] > (i + 1) / 2) {
+                    p2[i] = t;
+                }
+            }
+        }
+
+
+
+        for (int i = 0; i < n; ++i) {
+            cout << p1[i] << " ";
+        }
+        cout<<endl;
+        for (int i = 0; i < n; ++i) {
+            cout << p2[i] << " ";
+        }
+
+        for (int i=0;i<n-1;++i){
+            if  (p1[i]==p2[n-i-2] && p1[i]!=-1){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
