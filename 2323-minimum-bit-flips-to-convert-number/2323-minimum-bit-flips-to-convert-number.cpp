1571440e066e6dddc936c4fbbf77994f882305de
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2025-03-27 17:20:15 +0530

    Time: 0 ms (100%), Space: 7.9 MB (91.12%) - LeetHub

---
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minBitFlips(int x, int y) {
+        int ans = 0;
+        for (int i = 0; i < 32; ++i) {
+            int a = (x >> i) & 1;
+            int b = (y >> i) & 1;
+            ans += a != b;
+        }
+        return ans;
+    }
+};
