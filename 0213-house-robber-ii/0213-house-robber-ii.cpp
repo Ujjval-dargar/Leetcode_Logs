46572af13686c214ca01c903af3695600cb528a1
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2025-06-14 23:05:29 +0530

    Time: 0 ms (100%), Space: 10.9 MB (21.28%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-
+    int helper(vector<int>& nums) {
         int n = nums.size();
-        if (n==1){
+
+        int take = nums[0];
+        int skip = 0;
+        for (int i = 1; i < n; ++i) {
+            int newTake = nums[i] + skip;
+            int newSkip = max(take, skip);
+
+            take = newTake;
+            skip = newSkip;
+        }
+
+        return max(take, skip);
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        if (n == 1)
             return nums[0];
-        }
 
-        vector<pair<int, int>> dp1(n, {-1, -1});
-        vector<pair<int, int>> dp2(n, {-1, -1});
+        vector<int> nums1(nums.begin(), nums.end() - 1);
+        vector<int> nums2(nums.begin() + 1, nums.end());
 
-        dp1[0] = {0, nums[0]};
-        for (int i = 1; i < n - 1; ++i) {
-            dp1[i] = {max(dp1[i - 1].first, dp1[i - 1].second),
-                      nums[i] + dp1[i - 1].first};
-        }
-
-        dp2[1] = {0, nums[1]};
-        for (int i = 2; i < n; ++i) {
-            dp2[i] = {max(dp2[i - 1].first, dp2[i - 1].second),
-                      nums[i] + dp2[i - 1].first};
-        }
-
-        return max(max(dp1[n - 2].first, dp1[n - 2].second),
-                   max(dp2[n - 1].first, dp2[n - 1].second));
+        return max(helper(nums1), helper(nums2));
     }
 };
