5a2380c980ff0c9e911d2309fa5f84513ab5b00f
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2025-06-16 16:19:31 +0530

    Time: 65 ms (11.03%), Space: 49.8 MB (19.57%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
+    int f(vector<int>& coins, int amount, int i, long long curr,
+          vector<vector<int>>& mem) {
+
+        if (curr == amount)
+            return 0;
+        if (curr > amount)
+            return INT_MAX;
+
+        int n = coins.size();
+        if (i == n)
+            return INT_MAX;
+
+        if (mem[i][curr] != -1)
+            return mem[i][curr];
+
+        return mem[i][curr] =
+                   min(1LL + f(coins, amount, i, 0LL + curr + coins[i], mem),
+                       (long long)f(coins, amount, i + 1, curr, mem));
+    }
+
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount + 1, INT_MAX);
-        dp[0] = 0;
+        int n = coins.size();
+        vector<vector<int>> mem(n, vector<int>(amount + 1, -1));
 
-        for (int i = 1; i <= amount; ++i) {
-            for (int j = 0; j < coins.size(); ++j) {
-                if (i - coins[j] >= 0 && dp[i - coins[j]] != INT_MAX) {
-                    dp[i] = min(dp[i], 1 + dp[i - coins[j]]);
-                }
-            }
-        }
-
-        return (dp[amount] != INT_MAX ? dp[amount] : -1);
+        int ans = f(coins, amount, 0, 0, mem);
+        return ans == INT_MAX ? -1 : ans;
     }
 };
