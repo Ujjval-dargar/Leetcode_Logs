64bf4802f399bad73d38a4222239889a1a85ddb6
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2025-06-16 15:32:56 +0530

    Time: 23 ms (6.61%), Space: 47 MB (9.98%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        priority_queue<int, vector<int>, greater<int>> greed(g.begin(), g.end());
-        priority_queue<int, vector<int>, greater<int>> sze(s.begin(), s.end());
+        priority_queue<int, vector<int>, greater<>> greed(g.begin(), g.end());
+        priority_queue<int, vector<int>, greater<>> sz(s.begin(), s.end());
 
         int ans = 0;
-        while (!greed.empty() && !sze.empty()){
-            int a = greed.top();
-            int b = sze.top();
-
-            if (b<a){
-                sze.pop();
-            }else {
+        while (!greed.empty() && !sz.empty()) {
+            if (sz.top() < greed.top()) {
+                sz.pop();
+            } else {
                 ans++;
+                sz.pop();
                 greed.pop();
-                sze.pop();
             }
         }
 
