993d670f3cf7d5fb65465f4328d05b12ea5a4b44
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2025-06-18 15:50:43 +0530

    Time: 11 ms (65.64%), Space: 12.5 MB (76.2%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
+        int n = nums.size();
         sort(nums.begin(), nums.end());
 
-        int n = nums.size();
-        vector<int> dp(n);
+        vector<int> dp(n, 1);
         vector<int> next(n, -1);
 
         for (int i = n - 1; i >= 0; --i) {
-            dp[i] = 1;
             for (int j = i + 1; j < n; ++j) {
-                if (nums[j] % nums[i] == 0 && dp[i] < 1 + dp[j]) {
-                    dp[i] = 1 + dp[j];
-                    next[i] = j;
+                if (nums[j] % nums[i] == 0) {
+                    if (dp[j] + 1 > dp[i]) {
+                        dp[i] = 1 + dp[j];
+                        next[i] = j;
+                    }
                 }
             }
         }
 
+        int mxLen = 0;
         int strt = -1;
-        int mx = INT_MIN;
         for (int i = 0; i < n; ++i) {
-            if (dp[i] > mx) {
-                mx = dp[i];
+            if (dp[i] > mxLen) {
+                mxLen = dp[i];
                 strt = i;
             }
         }
 
         vector<int> ans;
-        while (strt != -1){
+        while (strt != -1) {
             ans.push_back(nums[strt]);
             strt = next[strt];
         }
