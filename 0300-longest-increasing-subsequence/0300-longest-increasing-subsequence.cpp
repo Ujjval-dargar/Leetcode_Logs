8d76546356f6eaaaf6edaa206436c18f1a3937be
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2025-06-18 14:16:17 +0530

    Time: 3 ms (81.07%), Space: 14 MB (87.38%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,5 +1,25 @@
 class Solution {
 public:
+    // Binary Search - O(n log n)
+
+    int lengthOfLIS(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> lis;
+        for (int i = 0; i < n; ++i) {
+            if (lis.empty() || lis.back() < arr[i]) {
+                lis.push_back(arr[i]);
+            } else {
+                int idx =
+                    lower_bound(lis.begin(), lis.end(), arr[i]) - lis.begin();
+                lis[idx] = arr[i];
+            }
+        }
+
+        return lis.size();
+    }
+
+    // DP - O(n^2)
+
     // int f(vector<int>& arr, int i, int prev, vector<vector<int>>& mem) {
     //     int n = arr.size();
     //     if (i == n)
@@ -17,21 +37,21 @@ public:
     //     return mem[i][prev] = max(take, notTake);
     // }
 
-    int lengthOfLIS(vector<int>& arr) {
-        int n = arr.size();
-        vector<vector<int>> mem(n + 1, vector<int>(n + 1, 0));
+    // int lengthOfLIS(vector<int>& arr) {
+    //     int n = arr.size();
+    //     vector<vector<int>> mem(n + 1, vector<int>(n + 1, 0));
 
-        for (int i = n - 1; i >= 0; --i) {
-            for (int prev = n; prev >= 0; --prev) {
-                int notTake = mem[i + 1][prev];
-                int take = 0;
-                if (prev == n || arr[prev] < arr[i]) {
-                    take = 1 + mem[i + 1][i];
-                }
+    //     for (int i = n - 1; i >= 0; --i) {
+    //         for (int prev = n; prev >= 0; --prev) {
+    //             int notTake = mem[i + 1][prev];
+    //             int take = 0;
+    //             if (prev == n || arr[prev] < arr[i]) {
+    //                 take = 1 + mem[i + 1][i];
+    //             }
 
-                mem[i][prev] = max(take, notTake);
-            }
-        }
-        return mem[0][n];
-    }
+    //             mem[i][prev] = max(take, notTake);
+    //         }
+    //     }
+    //     return mem[0][n];
+    // }
 };
