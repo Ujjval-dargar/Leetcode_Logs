c13bb0fc50402728b2165ba2efd46ad86dcaa51c
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2025-06-20 14:55:52 +0530

    Time: 3 ms (30.67%), Space: 18.4 MB (5.32%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,52 +1,50 @@
 class Solution {
 public:
+    bool isValid(int i, int j, int m, int n) {
+        return (i >= 0 && i < m && j >= 0 && j < n);
+    }
+
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
 
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
-        queue<pair<pair<int, int>, int>> q;
-
+        queue<vector<int>> q;
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                if (grid[i][j] == 2) {
-                    q.push({{i, j}, 0});
-                }
+                if (grid[i][j] == 2)
+                    q.push({i, j, 0});
             }
         }
 
         int ans = 0;
-
+        vector<vector<int>> dirn = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
         while (!q.empty()) {
-            pair<pair<int, int>, int> p = q.front();
+            vector<int> tp = q.front();
             q.pop();
 
-            int i = p.first.first;
-            int j = p.first.second;
-            int t = p.second;
+            int i = tp[0];
+            int j = tp[1];
+            int t = tp[2];
+            ans = max(t, ans);
 
             visited[i][j] = true;
-
-            vector<vector<int>> dirn = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
             for (int k = 0; k < 4; ++k) {
                 int ni = i + dirn[k][0];
                 int nj = j + dirn[k][1];
 
-                if (ni < m && nj < n && ni >= 0 && nj >= 0 &&
-                    !visited[ni][nj] && grid[ni][nj] == 1) {
+                if (isValid(ni, nj, m, n) && !visited[ni][nj] &&
+                    grid[ni][nj] == 1) {
                     grid[ni][nj] = 2;
-                    q.push({{ni, nj}, t + 1});
-                    ans = max(ans, t + 1);
+                    q.push({ni, nj, t + 1});
                 }
             }
         }
 
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                if (grid[i][j] == 1) {
+                if (grid[i][j] == 1)
                     return -1;
-                }
             }
         }
 
