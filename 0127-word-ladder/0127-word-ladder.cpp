3e7669e0483097c0c801370575ea95a5a8b6e915
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2025-06-20 22:06:24 +0530

    Time: 575 ms (20.21%), Space: 46.1 MB (7.92%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,51 +1,68 @@
 class Solution {
 public:
-    int checkDiff(string a, string b) {
-        int n = a.size();
+    bool diffby1(string& s1, string& s2) {
+        int n = s1.size();
         int cnt = 0;
         for (int i = 0; i < n; ++i) {
-            if (a[i] != b[i])
-                cnt++;
+            cnt += s1[i] != s2[i];
+            if (cnt > 1)
+                return false;
         }
-        return cnt;
+
+        return cnt == 1;
     }
 
     int ladderLength(string beginWord, string endWord,
                      vector<string>& wordList) {
-        
-        bool found = false;
-        for (string temp:wordList){
-            if (temp==endWord){
-                found=true;
-                break;
+
+        bool found_begin = false;
+        bool found_end = false;
+        for (string s : wordList) {
+            if (s == endWord)
+                found_end = true;
+            if (s == beginWord)
+                found_begin = true;
+        }
+
+        if (!found_end)
+            return 0;
+
+        if (!found_begin)
+            wordList.push_back(beginWord);
+
+        int n = wordList.size();
+
+        unordered_map<string, vector<string>> adj;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i != j && diffby1(wordList[i], wordList[j])) {
+                    adj[wordList[i]].push_back(wordList[j]);
+                    adj[wordList[j]].push_back(wordList[i]);
+                }
             }
         }
 
-        if (!found)return 0;
-        
         unordered_map<string, bool> visited;
-
         queue<pair<string, int>> q;
         q.push({beginWord, 1});
         visited[beginWord] = true;
 
         while (!q.empty()) {
-            string s = q.front().first;
-            int l = q.front().second;
+            string u = q.front().first;
+            int dist = q.front().second;
             q.pop();
-            
 
-            if (s == endWord) {
-                return l;
-            }
+            if (u == endWord)
+                return dist;
 
-            for (string s1 : wordList) {
-                if (!visited[s1] && checkDiff(s1, s) == 1) {
-                    q.push({s1, l + 1});
-                    visited[s1]=true;
+            for (string v : adj[u]) {
+                if (!visited[v]) {
+                    q.push({v, dist + 1});
+                    visited[v] = true;
                 }
             }
         }
+
         return 0;
     }
 };
