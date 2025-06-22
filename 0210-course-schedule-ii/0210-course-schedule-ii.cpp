d02868d02aeab0e8d4cb06490a4a1ed7973f9622
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2025-06-22 21:57:46 +0530

    Time: 10 ms (15.01%), Space: 19.2 MB (15.43%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = numCourses;
+        unordered_map<int, vector<int>> adj;
+
+        for (auto i : prerequisites) {
+            adj[i[1]].push_back(i[0]);
+        }
+
+        vector<int> indegree(n, 0);
+        for (int u = 0; u < n; ++u) {
+            for (int v : adj[u]) {
+                indegree[v]++;
+            }
+        }
+
+        queue<int> q;
+        for (int u = 0; u < n; ++u) {
+            if (indegree[u] == 0)
+                q.push(u);
+        }
+
+        vector<int> order;
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            order.push_back(u);
+
+            for (int v : adj[u]) {
+                indegree[v]--;
+                if (indegree[v] == 0)
+                    q.push(v);
+            }
+        }
+
+        int o = order.size();
+        if (o != n)
+            return {};
+        return order;
+    }
+};
