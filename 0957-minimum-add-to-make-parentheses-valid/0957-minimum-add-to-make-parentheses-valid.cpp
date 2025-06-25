84e4143900b88e12a356ff4657165845eba663ec
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2025-06-26 01:30:00 +0530

    Time: 0 ms (100%), Space: 8.3 MB (79.4%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        int ans = 0;
-        int n = s.size();
+        stack<char> stk;
 
-        stack<int> stk;
-        for (int i = 0; i < n; ++i) {
-            char ch = s[i];
-            if (ch == '(')
-                stk.push(ch);
-            else {
-                if (stk.empty()) {
-                    ans++;
-                } else {
-                    stk.pop();
-                }
+        int ans = 0;
+        for (char ch:s){
+            if (ch=='(') stk.push(ch);
+            else{
+                if (!stk.empty()) stk.pop();
+                else ans++;
             }
         }
 
