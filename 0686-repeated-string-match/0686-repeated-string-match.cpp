1e4b7028df4c33d79404d2d757febbf684d442c7
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2025-06-26 18:40:11 +0530

    Time: 0 ms (100%), Space: 10 MB (52.38%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.cpp
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -1,74 +1,63 @@
 class Solution {
 public:
-    // bool rabinKarp(string txt, string pat) {
-    //     int n = txt.size();
-    //     int m = pat.size();
+    bool rabinKarp(string txt, string pat) {
+        int n = txt.size();
+        int m = pat.size();
+        if (m > n) return false;
 
-    //     if (m > n)
-    //         return false;
+        long long d   = 256;
+        long long mod = 1000000007;
+        
+        long long dm = 1;
+        for (int i = 0; i < m; ++i)
+            dm = (dm * d) % mod;
 
-    //     int d = 256;
-    //     int mod = 1e9 + 7;
-    //     long long dm = 1;
-    //     for (int i = 0; i < m - 1; ++i)
-    //         dm = (dm * d) % mod;
+        long long targetHash = 0;
+        for (unsigned char ch : pat)
+            targetHash = (targetHash * d + ch) % mod;
 
-    //     long long targetHash = 0;
-    //     for (char ch : pat) {
-    //         targetHash = (targetHash * d + ch) % mod;
-    //     }
+        long long currHash = 0;
+        int l = 0, r = 0;
+        while (r < n) {
+            
+            currHash = (currHash * d + txt[r]) % mod;
 
-    //     long long currHash = 0;
-    //     int l = 0;
-    //     int r = 0;
-    //     while (r < n) {
+            if (r - l + 1 > m) {
+                currHash = (
+                    currHash
+                    - (long long)txt[l] * dm % mod
+                    + mod
+                ) % mod;
+                l++;
+            }
+            if (r - l + 1 == m) {
+                if (currHash == targetHash) {
+                    bool match = true;
+                    for (int i = 0; i < m; ++i) {
+                        if (txt[l + i] != pat[i]) {
+                            match = false;
+                            break;
+                        }
+                    }
+                    if (match) return true;
+                }
+            }
 
-    //         if (r - l + 1 < m) {
-    //             currHash = (currHash * d + txt[r]) % mod;
-    //         }
-
-    //         else if (r - l + 1 == m) {
-    //             bool flag = false;
-    //             for (int i = 0; i < m; ++i) {
-    //                 if (txt[l + i] != pat[i]) {
-    //                     flag = true;
-    //                     break;
-    //                 }
-    //             }
-
-    //             if (!flag)
-    //                 return true;
-
-    //             currHash = (((currHash - txt[l] * dm + mod) % mod * d) % mod
-    //             +
-    //                         txt[r]) %
-    //                        mod;
-    //             l++;
-    //         }
-    //         r++;
-    //     }
-
-    //     return false;
-    // }
+            r++;
+        }
+        return false;
+    }
 
     int repeatedStringMatch(string a, string b) {
-        if (a == b)
-            return 1;
-
+        if (a == b) return 1;
         string org = a;
         int cnt = 1;
         while (a.size() < b.size()) {
             a += org;
             cnt++;
         }
-
-        if (a.find(b) != string::npos)
-            return cnt;
-
-        a += org;
-        if (a.find(b) != string::npos)
-            return cnt + 1;
-
+        if (rabinKarp(a, b))         return cnt;
+        if (rabinKarp(a + org, b))   return cnt + 1;
         return -1;
     }
 };
