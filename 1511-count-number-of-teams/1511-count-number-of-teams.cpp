4751e3796c44344e496666a68f0aaee8174fcd84
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2025-06-27 12:36:06 +0530

    Time: 23 ms (71.63%), Space: 12.6 MB (57.45%) - LeetHub

--- 1511-count-number-of-teams/1511-count-number-of-teams.cpp
+++ 1511-count-number-of-teams/1511-count-number-of-teams.cpp
@@ -2,21 +2,30 @@ class Solution {
 public:
     int numTeams(vector<int>& rating) {
         int ans = 0;
-
         int n = rating.size();
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                for (int k = j + 1; k < n; ++k) {
-                    if ((rating[i] < rating[j] && rating[i] < rating[k] &&
-                         rating[j] < rating[k]) ||
-                        (rating[i] > rating[j] && rating[i] > rating[k] &&
-                         rating[j] > rating[k])
 
-                    ) {
-                        ans++;
-                    }
-                }
+        for (int j = 0; j < n; ++j) {
+            int l1 = 0;
+            int r1 = 0;
+
+            int l2 = 0;
+            int r2 = 0;
+
+            for (int i = 0; i < j; ++i) {
+                if (rating[i] < rating[j])
+                    l1++;
+                if (rating[i] > rating[j])
+                    l2++;
             }
+
+            for (int k = j + 1; k < n; ++k) {
+                if (rating[j] < rating[k])
+                    r1++;
+                if (rating[j] > rating[k])
+                    r2++;
+            }
+
+            ans += (l1 * r1) + (l2 * r2);
         }
 
         return ans;
