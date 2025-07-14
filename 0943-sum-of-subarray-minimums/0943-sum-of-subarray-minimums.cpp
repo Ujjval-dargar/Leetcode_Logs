6161ac4aa4c461f935f4b0fec929de7ef138fc73
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2025-07-14 20:59:07 +0530

    Time: 5 ms (98.64%), Space: 43.6 MB (96.62%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
@@ -1,46 +1,22 @@
-// brute
-
-// class Solution {
-// public:
-//     int sumSubarrayMins(vector<int>& arr) {
-//         int n = arr.size();
-
-//         int mod = 1e9 + 7;
-//         int ans = 0;
-//         for (int i = 0; i < n; ++i){
-//             for (int j = i; j < n; ++j){
-//                 int len = j - i + 1;
-//                 int mn = *min_element(arr.begin() + i, arr.begin() + i +
-//                 len); ans = (ans + mn) % mod;
-//             }
-//         }
-
-//         return ans;
-//     }
-// };
-
-// -------------------------------------------------------------------------------------------------
-
-// Optimal
-
 class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
         int n = arr.size();
-
         int mod = 1e9 + 7;
-        int ans = 0;
-        stack<int> s;
 
-        for (int i = 0; i <= n; ++i) {
-            while (!s.empty() && (i == n || arr[s.top()] >= arr[i])) {
+        stack<int> s;
+        int ans = 0;
+        for (int i = 0; i <= n; ++i){
+            while (!s.empty() && (i == n || arr[s.top()] >= arr[i]))
+            {
                 int mid = s.top();
                 s.pop();
-                int left = s.empty() ? -1 : s.top();
 
-                ans =
-                    (ans + (1LL * (mid - left) * (i - mid) * arr[mid]) % mod) %
-                    mod;
+                int left = (s.empty())? -1 : s.top();
+                int right  = i;
+
+                int cont = (1LL * (mid - left) * (right - mid) * arr[mid]) % mod;
+                ans = (ans + cont) % mod;
             }
             s.push(i);
         }
