c86e7dead8f92c6721c1e023d40c7e8042fa5b3a
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2025-08-18 11:46:20 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98.71%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        return n > 0 && (n & (n - 1)) == 0 && (n & 0x55555555);
+
+        if (n == 1)
+            return true;
+        if (n <= 3)
+            return false;
+        int cnt = __builtin_popcount(n);
+
+        if (cnt > 1) {
+            return false;
+        }
+
+        bitset<32> bin_num = (bitset<32>(n));
+    
+        int pos = -1;
+        for (int i = 31; i >= 0; --i) {
+            if (bin_num[i] == 1 && !(i % 2)) {
+                pos = i;
+                break;
+            }
+        }
+        return !(pos % 2);
     }
 };
