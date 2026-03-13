68e12d51387029f5e8137c23c32c3adf3d7197f6
Ujjval Dargar <ujjval23564@iiitd.ac.in>
2026-03-13 17:03:07 +0530

    Time: 22 ms (62.58%), Space: 29 MB (43.87%) - LeetHub

--- 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
+++ 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
@@ -2,32 +2,31 @@ class Solution {
 public:
     bool check(long long mid, int h, vector<int>& workerTimes){
         
-        for (int t : workerTimes){
+        int n = workerTimes.size();
+        for (int i = 0; i < n; ++i){
             if (h <= 0) return true;
-
-            long double val = (long double)2 * mid / t;
-            long long x = (sqrtl(1 + 4 * val) - 1) / 2;
-
+            long long t = workerTimes[i];
+            long long val = (2 * mid) / t;
+            long long x = (sqrt(1 + 4 * val) - 1) / 2;
             h -= x;
         }
 
-        return h <= 0;
+        return h <= 0;   
     }
 
     long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
         long long l = 0;
         long long r = 1e18;
 
-        long long ans = r;
-
-        while (l <= r){
-            long long mid = l + (r - l) / 2;
-
+        long long ans;
+        while (l <= r)
+        {
+            long long mid = (r - l)/2 + l;
             if (check(mid, mountainHeight, workerTimes)){
-                ans = mid;
                 r = mid - 1;
-            } 
-            else{
+                ans = mid;
+            }
+            else {
                 l = mid + 1;
             }
         }
